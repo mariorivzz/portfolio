5fd4959550eab47ac8e2dc36b8ae81818f84de35
mariorivzz <mariomariorivasrivas@gmail.com>
2026-05-08 21:30:59 +0200

    feat: sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://mariorivasrivas.com',
   // 'static' = static by default, API routes opt out via `export const prerender = false`
   output: 'static',
+  integrations: [sitemap()],
   adapter: vercel(),
 });
