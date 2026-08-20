44f699147a20f1a259dc5bc7808ece282a72648c
mariorivzz <mariomariorivasrivas@gmail.com>
2026-08-20 15:52:40 +0200

    seo

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://portfolio-mariorivas.vercel.app',
+  site: 'https://mariorivashernandez.com',
   // 'static' = static by default, API routes opt out via `export const prerender = false`
   output: 'static',
   integrations: [sitemap({
